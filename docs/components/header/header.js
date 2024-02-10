e501664052f988bde0755eb771d40cd9a7587cab
andrewdelamar <52581649+andrewdelamar@users.noreply.github.com>
2024-02-10 18:36:06 -0500

    Flatten images

--- docs/components/header/header.js
+++ docs/components/header/header.js
@@ -10,7 +10,7 @@ headerTemplate.innerHTML = `
         <span class="wsite-logo">
           <a href="">
             <img
-              src="uploads/3/5/0/5/35055391/published/1369226295.png?1496405773"
+              src="images/published/1369226295.png?1496405773"
               alt="Spa Tec"
             />
           </a>
