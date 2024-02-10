0728e202bc2b7ef8cfc34950417f2572733c1584
andrewdelamar <52581649+andrewdelamar@users.noreply.github.com>
2024-02-10 16:24:32 -0500

    Fix header component

--- docs/header.js
+++ docs/header.js
@@ -1,6 +1,32 @@
 const headerTemplate = document.createElement("template");
 headerTemplate.innerHTML = `
 
+<style>
+  #header {
+    background: #495561 url(theme/header_background.gif?1527348198) repeat-x
+      center top;
+    width: 920px;
+    font-weight: normal;
+    padding-top: 6px;
+    padding-right: 20px;
+    padding-left: 20px;
+    border-bottom: 1px solid #0c0f12;
+  }
+  #header-inner {
+    width: 100%;
+    height: 90px;
+  }
+  #header-inner,
+  #header-inner table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  #header-inner td {
+    vertical-align: middle;
+    text-align: left;
+  }
+</style>
+
 <div id="header">
   <table id="header-inner">
     <tr>
@@ -26,8 +52,8 @@ class Header extends HTMLElement {
   }
 
   connectedCallback() {
-    const temp = document.importNode(headerTemplate.content, true);
-    this.appendChild(temp);
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(headerTemplate.content);
   }
 }
 
